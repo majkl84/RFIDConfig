82c67aed3fcf23427060d9c7fc7ed759f8472bba
majkl84 <34282210+majkl84@users.noreply.github.com>
2023-07-20 14:13:36 +0300

    Полностью актуально Настройка параметров RFID-тракта (rfidconfig)

--- SAUKRFIDConfig.py
+++ SAUKRFIDConfig.py
@@ -28,39 +28,39 @@ class RFIDConfig:
         return response
 
     def set_enable_antenna(self, value):
-        url = f"{self.base_url}/rfidconfig?enant1=bool"
-        data = {"enant1": value}
-        response = requests.get(url, data=data)
+        url = f"{self.base_url}/rfidconfig?enant1={str(value).lower()}"
+        response = requests.get(url, auth=(self.login, self.password))
+        response.raise_for_status()
         return response
 
     def set_enable_trigger(self, value):
-        url = f"{self.base_url}/rfidconfig?entrig1=bool"
-        data = {"entrig1": value}
-        response = requests.get(url, data=data)
+        url = f"{self.base_url}/rfidconfig?entrig1={str(value).lower()}"
+        response = requests.get(url, auth=(self.login, self.password))
+        response.raise_for_status()
         return response
 
     def set_trigger_state(self, value):
-        url = f"{self.base_url}/rfidconfig?triggered1=value"
-        data = {"triggered1": value}
-        response = requests.get(url, data=data)
+        url = f"{self.base_url}/rfidconfig?triggered1={value}"
+        response = requests.get(url, auth=(self.login, self.password))
+        response.raise_for_status()
         return response
 
     def set_antenna_dependency(self, value):
-        url = f"{self.base_url}/rfidconfig?rf_session=value"
-        data = {"rf_session": value}
-        response = requests.get(url, data=data)
+        url = f"{self.base_url}/rfidconfig?rf_session={value}"
+        response = requests.get(url, auth=(self.login, self.password))
+        response.raise_for_status()
         return response
 
     def set_repeattime(self, value):
-        url = f"{self.base_url}/rfidconfig?repeattime=value"
-        data = {"repeattime": value}
-        response = requests.get(url, data=data)
+        url = f"{self.base_url}/rfidconfig?repeattime={value}"
+        response = requests.get(url, auth=(self.login, self.password))
+        response.raise_for_status()
         return response
 
     def set_min_hold_ms(self, value):
-        url = f"{self.base_url}/rfidconfig?min_hold_ms=value"
-        data = {"min_hold_ms": value}
-        response = requests.get(url, data=data)
+        url = f"{self.base_url}/rfidconfig?min_hold_ms={value}"
+        response = requests.get(url, auth=(self.login, self.password))
+        response.raise_for_status()
         return response
 
 ################################################################
--- Test1.py
+++ Test1.py
@@ -8,8 +8,38 @@ if __name__ == '__main__':
     # response = rfid_config.set_continuous_scanning(True)
     # print(response.status_code)
 
-    response = rfid_config.set_power_antenna(19)
+    # response = rfid_config.set_power_antenna(19)
+    # if response.status_code == 200:
+    #     print("Power antenna was set successfully!")
+    # else:
+    #     print("Error setting power antenna.")
+
+    # response = rfid_config.set_enable_antenna(True)
+    # print(response.status_code)
+
+    # response = rfid_config.set_enable_trigger(True)
+    # print(response.status_code)
+
+    # response = rfid_config.set_trigger_state(1)
+    # if response.status_code == 200:
+    #     print("Trigger state was set successfully!")
+    # else:
+    #     print("Error setting trigger state.")
+
+    # response = rfid_config.set_antenna_dependency(4)
+    # if response.status_code == 200:
+    #     print("Antenna dependency was set successfully!")
+    # else:
+    #     print("Error setting antenna dependency.")
+
+    # response = rfid_config.set_repeattime(2)
+    # if response.status_code == 200:
+    #     print("Repeattime was set successfully!")
+    # else:
+    #     print("Error setting repeattime.")
+
+    response = rfid_config.set_min_hold_ms(5000)
     if response.status_code == 200:
-        print("Power antenna was set successfully!")
+        print("Min hold time was set successfully!")
     else:
-        print("Error setting power antenna.")
+        print("Error setting min hold time.")
