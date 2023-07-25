915562206b5f956b1ab17f89fad0481868e18366
majkl84 <34282210+majkl84@users.noreply.github.com>
2023-07-25 11:48:53 +0300

    Приводим репозиторий в соответствие.

--- RFIDConfig.py
+++ RFIDConfig.py
@@ -1,4 +1,7 @@
 import requests
+import socket
+from urllib.parse import urlparse
+import sys
 
 
 class ApiClient:
@@ -8,123 +11,138 @@ class ApiClient:
         self.login = login
         self.password = password
 
+    def _check_base_url(self):
+        url = urlparse(self.base_url)
+        host = url.netloc
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.settimeout(2)
+            sock.connect((host, url.port if url.port else 80))
+
     def get(self, endpoint):
         url = f"{self.base_url}/{endpoint}"
-        return requests.get(url, auth=(self.login, self.password))
+        try:
+            response = requests.get(url, auth=(self.login, self.password))
+            response.raise_for_status()
+        except requests.RequestException as err:
+            print(f"Ошибка при GET запросе: {err}")
+            sys.exit(1)
+
+        return response.json()
 
     def set(self, endpoint, params):
         url = f"{self.base_url}/{endpoint}"
-        return requests.get(url, params=params, auth=(self.login, self.password))
+        try:
+            response = requests.get(url, params=params, auth=(self.login, self.password))
+            response.raise_for_status()
+        except requests.RequestException as err:
+            print(f"Ошибка при SET запросе: {err}")
+            sys.exit(1)
+
+        return response.json()
 
 
 class RFIDConfig(ApiClient):
 
     def get_params(self):
-        return self.get("rfidconfig").json()
+        return self.get("rfidconfig")
 
     def set_continuous_scanning(self, value):
-        return self.set("rfidconfig", {"infiniteinventory": str(value).lower()}).json()
+        return self.set("rfidconfig", {"infiniteinventory": str(value).lower()})
 
     def set_power_antenna(self, value, ch):
-        params = {f"pwrant{ch}": value}
-        return self.set("rfidconfig", params).json()
+        return self.set("rfidconfig", {f"pwrant{ch}": value})
 
     def set_enable_antenna(self, value, ch):
-        params = {f"enant{ch}": str(value).lower()}
-        return self.set("rfidconfig", params).json()
+        return self.set("rfidconfig", {f"enant{ch}": str(value).lower()})
 
     def set_enable_trigger(self, value, ch):
-        params = {f"entrig{ch}": str(value).lower()}
-        return self.set("rfidconfig", params).json()
+        return self.set("rfidconfig", {f"entrig{ch}": str(value).lower()})
 
-####????????
+    ####????????
     def set_trigger_state(self, value, ch):
-        params = {f"triggered{ch}": value}
-        return self.set("rfidconfig", params).json()
+        return self.set("rfidconfig", {f"triggered{ch}": value})
 
     def set_antenna_dependency(self, value):
-        return self.set("rfidconfig", {"rf_session": value}).json()
+        return self.set("rfidconfig", {"rf_session": value})
 
     def set_repeattime(self, value):
-        return self.set("rfidconfig", {"repeattime": value}).json()
+        return self.set("rfidconfig", {"repeattime": value})
 
     def set_min_hold_ms(self, value):
-        return self.set("rfidconfig", {"min_hold_ms": value}).json()
+        return self.set("rfidconfig", {"min_hold_ms": value})
 
 
 class PeripheryConfig(ApiClient):
 
     def get_params(self):
-        return self.get("peripheryconfig").json()
+        return self.get("peripheryconfig")
 
     def set_relay_unit_enable(self, value):
-        return self.set("peripheryconfig", {"smartboard_enable": str(value).lower()}).json()
+        return self.set("peripheryconfig", {"smartboard_enable": str(value).lower()})
 
     def set_relay_enable(self, value, ch):
-        params = {f"smartboard_port{ch}_enable": str(value).lower()}
-        return self.set("peripheryconfig", params).json()
+        return self.set("peripheryconfig", {f"smartboard_port{ch}_enable": str(value).lower()})
+
+    def set_relay_enable_ant(self, value, ch):
+        return self.set("peripheryconfig", {f"smartboard_port{ch}_ants": str(value).lower()})
 
     def set_relay_timer(self, value, ch):
-        params = {f"smartboard_port{ch}_timer": str(value).lower()}
-        return self.set("peripheryconfig", params).json()
+        return self.set("peripheryconfig", {f"smartboard_port{ch}_timer": str(value).lower()})
 
     def set_wiegand_enable(self, value, ch):
-        params = {f"wiegand{ch}_enable": str(value).lower()}
-        return self.set("peripheryconfig", params).json()
+        return self.set("peripheryconfig", {f"wiegand{ch}_enable": str(value).lower()})
 
     def set_wiegand_type(self, value):
-        return self.set("peripheryconfig", {"wiegand1_type": value}).json()
+        return self.set("peripheryconfig", {"wiegand1_type": value})
 
     def set_wiegand_shift_bytes(self, value):
-        return self.set("peripheryconfig", {"wiegand1_shift_bytes": value}).json()
+        return self.set("peripheryconfig", {"wiegand1_shift_bytes": value})
 
     def set_wiegand_source(self, value):
-        return self.set("peripheryconfig", {"wiegand1_source": value}).json()
+        return self.set("peripheryconfig", {"wiegand1_source": value})
 
     def set_beep_on_start(self, value):
-        return self.set("peripheryconfig", {"beep_on_start": str(value).lower()}).json()
+        return self.set("peripheryconfig", {"beep_on_start": str(value).lower()})
 
     def set_timeout_logical(self, value):
-        return self.set("peripheryconfig", {"timeout_logical_0": value}).json()
+        return self.set("peripheryconfig", {"timeout_logical_0": value})
 
     def set_timeout_next_bit(self, value):
-        return self.set("peripheryconfig", {"timeout_next_bit": value}).json()
+        return self.set("peripheryconfig", {"timeout_next_bit": value})
 
 
 class TagIdentity(ApiClient):
 
     def get_params(self):
-        return self.get("tagidentity").json()
+        return self.get("tagidentity")
 
     def get_tag_list(self):
-        return self.get("tagidentity?taglist=true").json()
+        return self.get("tagidentity?taglist=true")
 
     def set_valid_time_ms(self, value):
-        return self.set("tagidentity", {"validtime_ms": value}).json()
+        return self.set("tagidentity", {"validtime_ms": value})
 
     ###????
     def set_hold_time_ms(self, value):
-        return self.set("tagidentity", {"hold_time_ms": value}).json()
+        return self.set("tagidentity", {"hold_time_ms": value})
 
     def set_rssi_filter_value(self, value):
-        return self.set("tagidentity", {"rssi_filter_value": -value}).json()
+        return self.set("tagidentity", {"rssi_filter_value": -value})
 
     def set_rssi_filter_enable(self, value):
-        return self.set("tagidentity", {"rssi_filter_enable": str(value).lower()}).json()
+        return self.set("tagidentity", {"rssi_filter_enable": str(value).lower()})
 
     def set_epc_access_password(self, value):
-        return self.set("tagidentity", {"epc_access_password": value}).json()
+        return self.set("tagidentity", {"epc_access_password": value})
 
     def set_epc_filter_value(self, value, filter):
-        params = {f"epc_filter_value{filter}": str(value).lower()}.json()
-        return self.set("tagidentity", params)
+        return self.set("tagidentity", {f"epc_filter_value{filter}": str(value).lower()})
 
     def set_epc_filter_enable(self, value, filter):
-        params = {f"epc_filter_enable{filter}": str(value).lower()}.json()
-        return self.set("tagidentity", params)
+        return self.set("tagidentity", {f"epc_filter_enable{filter}": str(value).lower()})
 
     def set_beep_on_tag(self, value):
-        return self.set("tagidentity", {"beep_on_tag": str(value).lower()}).json()
+        return self.set("tagidentity", {"beep_on_tag": str(value).lower()})
 
     # def set_extra_mem_read(self, value):
     #     url = f"{self.base_url}/tagidentity?extra_mem_read=bool"
@@ -151,74 +169,73 @@ class TagIdentity(ApiClient):
     #     return response
 
     def set_notify_uart(self, value):
-        return self.set("tagidentity", {"notify_uart": str(value).lower()}).json()
+        return self.set("tagidentity", {"notify_uart": str(value).lower()})
 
     def set_notify_uart_json(self, value):
-        return self.set("tagidentity", {"notify_uart_json": value}).json()
+        return self.set("tagidentity", {"notify_uart_json": value})
 
     def set_add_prefix(self, value):
-        return self.set("tagidentity", {"add_prefix": value}).json()
+        return self.set("tagidentity", {"add_prefix": value})
 
     def set_add_epcl(self, value):
-        return self.set("tagidentity", {"add_epcl": str(value).lower()}).json()
+        return self.set("tagidentity", {"add_epcl": str(value).lower()})
 
     def set_add_epc(self, value):
-        return self.set("tagidentity", {"add_epc": str(value).lower()}).json()
+        return self.set("tagidentity", {"add_epc": str(value).lower()})
 
     def set_add_tidl(self, value):
-        return self.set("tagidentity", {"add_tidl": str(value).lower()}).json()
+        return self.set("tagidentity", {"add_tidl": str(value).lower()})
 
     def set_add_tid(self, value):
-        return self.set("tagidentity", {"add_tid": str(value).lower()}).json()
+        return self.set("tagidentity", {"add_tid": str(value).lower()})
 
     # def set_add_suffix(self, value):
     #     return self.set("tagidentity", {"add_suffix": value})
 
     def set_add_crlf(self, value):
-        return self.set("tagidentity", {"add_crlf": str(value).lower()}).json()
+        return self.set("tagidentity", {"add_crlf": str(value).lower()})
 
     def set_add_ant(self, value):
-        return self.set("tagidentity", {"add_ant": str(value).lower()}).json()
+        return self.set("tagidentity", {"add_ant": str(value).lower()})
 
     def set_add_rssi(self, value):
-        return self.set("tagidentity", {"add_rssi": str(value).lower()}).json()
+        return self.set("tagidentity", {"add_rssi": str(value).lower()})
 
     def set_notify_uart_alive(self, value):
-        return self.set("tagidentity", {"notify_uart_alive": str(value).lower()}).json()
+        return self.set("tagidentity", {"notify_uart_alive": str(value).lower()})
 
     def set_notify_uart_speed(self, value):
-        return self.set("tagidentity", {"notify_uart_speed": value}).json()
+        return self.set("tagidentity", {"notify_uart_speed": value})
 
     def set_notify_ip(self, value):
-        return self.set("tagidentity", {"notify_ip": value}).json()
+        return self.set("tagidentity", {"notify_ip": value})
 
     def set_notify_port(self, value):
-        return self.set("tagidentity", {"notify_port": value}).json()
+        return self.set("tagidentity", {"notify_port": value})
 
     def set_notify_time_lim_ms(self, value):
-        return self.set("tagidentity", {"notify_time_lim_ms": value}).json()
+        return self.set("tagidentity", {"notify_time_lim_ms": value})
 
     def set_notify_enable(self, value):
-        return self.set("tagidentity", {"notify_enable": str(value).lower()}).json()
+        return self.set("tagidentity", {"notify_enable": str(value).lower()})
 
 
 class NetworkConfig(ApiClient):
 
     def get_netinfo(self):
-        return self.get("netinfo").json()
+        return self.get("netinfo")
 
     def set_sta_enable(self, value):
-        return self.set("netinfo", {"sta_enable": str(value).lower()}).json()
+        return self.set("netinfo", {"sta_enable": str(value).lower()})
 
     def set_ap_enable(self, value):
-        return self.set("netinfo", {"ap_enable": str(value).lower()}).json()
+        return self.set("netinfo", {"ap_enable": str(value).lower()})
 
     def set_wificonnect(self, ssid, password, safe):
-        data = {"ssid": ssid, "pass": password, "safe": safe}
-        return self.set("wificonnect", data).json()
+        return self.set("wificonnect", {"ssid": ssid, "pass": password, "safe": safe})
 
     def scan_wifi(self):
-        return self.get("scan").json()
+        return self.get("scan")
 
 
 class SystemCommands(ApiClient):
@@ -226,10 +243,10 @@ class SystemCommands(ApiClient):
         return self.get("logout")
 
     def get_messagelog(self):
-        return self.get("messagelog").json()
+        return self.get("messagelog")
 
     def get_version(self):
-        return self.get("version").json()
+        return self.get("version")
 
     def reboot(self):
         return self.get("reboot")
